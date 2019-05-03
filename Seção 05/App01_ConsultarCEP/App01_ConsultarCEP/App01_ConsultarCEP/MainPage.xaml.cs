864d0025b54382322f78e52f51d43694f5872669
Elton Barreto <eltonsc1@hotmail.com>
2019-05-03 14:32:49 -0300

    Criação do projeto ConsultarCEP

---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultarCEP.Servico.Modelo;
+using App01_ConsultarCEP.Servico;
+
+namespace App01_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Pesquisar.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            string cep = CEP.Text.Trim();
+
+            if (isValidCep(cep).Sucesso)
+            {
+                Endereco endereco = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                if (endereco != null)
+                {
+                    Rua.Text = endereco.logradouro;
+                    Bairro.Text = endereco.bairro;
+                    Cidade.Text = endereco.localidade;
+                    UF.Text = endereco.uf;
+                }
+                else
+                    DisplayAlert("Erro", string.Format("CEP não encontrado! ({0})", cep), "OK");
+
+            }
+        }
+
+        private Resultado isValidCep(string cep)
+        {
+            Resultado resultado = new Resultado() { Sucesso = true };
+            int novoCep = 0;
+
+            if (cep.Length != 8 || !int.TryParse(cep, out novoCep))
+            {
+                resultado.Sucesso = false;
+                DisplayAlert("Erro", "CEP inválido!", "OK");
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/Modelo/Resultado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App01_ConsultarCEP.Servico.Modelo
+{
+    public class Resultado
+    {
+        public string Mensagem { get; set; }
+        public bool Sucesso { get; set; }
+        public object Retorno { get; set; }
+    }
+}
